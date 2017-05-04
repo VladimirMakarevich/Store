d04e93857cc57080a02e6fb7ebc5f6674f4b2081
VladiMakarevich <justadreampictures@gmail.com>
2017-05-04 12:35:06 +0300

    removed unused namespace

--- Store.DAL/Context/StoreContext.cs
+++ Store.DAL/Context/StoreContext.cs
@@ -1,10 +1,5 @@
 ﻿using Store.DAL.Entities;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Store.DAL.Context
--- Store.DAL/Entities/PurchaseHistory.cs
+++ Store.DAL/Entities/PurchaseHistory.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Store.DAL.Entities
 {
     public class PurchaseHistory
     {
+        [Key]
         public int Id { get; set; }
 
         public State State { get; set; }
