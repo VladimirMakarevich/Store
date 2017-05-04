a614490bded728958924db531a4048489ebb155a
Vladimir Makarevich <business.makarevich@outlook.com>
2017-05-04 17:11:05 +0300

    added auto mapper config

---
+++ Store.Web/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ninject.Activation;
+
+namespace Store.Web.Mappers
+{
+    public class AutoMapperConfig
+    {
+        public static IMapper GetMapper(IContext context)
+        {
+            MapperConfiguration config = new MapperConfiguration(RegisterMappings);
+            IMapper mapper = config.CreateMapper();
+            return mapper;
+        }
+
+        private static void RegisterMappings(IMapperConfigurationExpression config)
+        {
+        }
+    }
+}
