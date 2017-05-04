940b3f700274a5b9bdb4dec85c1b7137c7fb8f1f
VladiMakarevich <justadreampictures@gmail.com>
2017-05-04 12:07:29 +0300

    added context to DAL

---
+++ Store.DAL/Context/StoreContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.DAL.Context
+{
+    public class StoreContext : IdentityDbContext<User>
+    {
+        public StoreContext()
+                : base("StoreDb")
+            {
+
+        }
+
+        public static StoreContext Create()
+        {
+            return new StoreContext();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            ConfigureCascadeDeleting(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void ConfigureCascadeDeleting(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
