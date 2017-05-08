6b03c50f99b757b2b1f492c9f86181ac90965687
Vladimir Makarevich <business.makarevich@outlook.com>
2017-05-08 12:33:52 +0300

    added some tests to ProductsController

--- Store.Web.Tests/Controllers/ProductsControllerTest.cs
+++ Store.Web.Tests/Controllers/ProductsControllerTest.cs
@@ -1,31 +1,60 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Store.Web.Tests.Ninject;
 using Store.Web.Controllers;
 using Ninject;
 using System.Threading.Tasks;
+using Store.BL.UnityOfWork;
+using System.Web.Http.Results;
+using Store.Web.Models;
 
 namespace Store.Web.Tests.Controllers
 {
     [TestClass]
     public class ProductsControllerTest : DependencyInjectedTest
     {
-        private ProductsController _controller;
+        private readonly IUnityOfWork _unityOfWork;
+        private readonly ProductsController _controller;
 
         public ProductsControllerTest()
         {
             NewScope();
             _controller = Kernel.Get<ProductsController>();
+            NewScope();
+            _unityOfWork = Kernel.Get<IUnityOfWork>();
         }
 
         [TestMethod]
-        public async Task Get_IsNotNull()
+        public async Task GetProducts_IsNotNull()
         {
             var result = await _controller.GetProducts();
 
             Assert.IsNotNull(result, "Result is null");
+        }
+
+        [TestMethod]
+        public async Task GetProduct_IsNotNull()
+        {
+            var product = await _unityOfWork.Products.GetAllAsync();
+            var idProduct = product.FirstOrDefault().Id;
+
+            var result = await _controller.GetProduct(idProduct) as OkNegotiatedContentResult<ProductJsonModel>;
+
+            Assert.IsNotNull(result, "Result is null");
+            Assert.AreEqual(idProduct, result.Content.Id, "Id products is not equal");
+        }
+
+        [TestMethod]
+        public async Task GetProduct_IsNull()
+        {
+            var fakeId = new Random().Next(1000000, 100000000);
+
+            var result = await _controller.GetProduct(fakeId) as OkNegotiatedContentResult<ProductJsonModel>;
+
+            Assert.IsNull(result, "Result is null");
         }
     }
 }
