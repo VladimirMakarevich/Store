bfa73bffe4fff03303e3f84e5147723f488153e0
Vladimir Makarevich <business.makarevich@outlook.com>
2017-05-08 11:22:34 +0300

    added test data to entities

--- Store.DAL/Migrations/Configuration.cs
+++ Store.DAL/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace Store.DAL.Migrations
 {
+    using Entities;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,18 +16,34 @@ namespace Store.DAL.Migrations
 
         protected override void Seed(Store.DAL.Context.StoreContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var category = new List<Category>
+            {
+                new Category { Name = "Category1", Description = "Description1" }
+            };
+            category.ForEach(s => context.Categories.Add(s));
+            context.SaveChanges();
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var product = new List<Product>
+            {
+                new Product { Name = "Product1", Description = "Description1", Price = 12, ImageData = null, ImageMimeType = null, Availability = Availability.InStock }
+            };
+            product.ForEach(s => context.Products.Add(s));
+            context.SaveChanges();
+
+            //for (int j = 1; j <= store.Count; ++j)
+            //{
+            //    for (int i = 0; i <= 20; i++)
+            //    {
+            //        var item = new Item
+            //        {
+            //            Name = $"Item{i} " + $"by {store[j - 1].Name}",
+            //            Description = $"Description{i} " + $"by {store[j - 1].Name}",
+            //            StoreId = j
+            //        };
+            //        context.Items.Add(item);
+            //        context.SaveChanges();
+            //    }
+            //}
         }
     }
 }
