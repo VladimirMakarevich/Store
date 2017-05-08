f68c2cdd4daf558ff846d114c721ca614715fc02
Vladimir Makarevich <business.makarevich@outlook.com>
2017-05-08 12:05:06 +0300

    added test for ProductController

---
+++ Store.Web.Tests/Controllers/ProductsControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Web.Tests.Ninject;
+using Store.Web.Controllers;
+using Ninject;
+using System.Threading.Tasks;
+
+namespace Store.Web.Tests.Controllers
+{
+    [TestClass]
+    public class ProductsControllerTest : DependencyInjectedTest
+    {
+        private ProductsController _controller;
+
+        public ProductsControllerTest()
+        {
+            NewScope();
+            _controller = Kernel.Get<ProductsController>();
+        }
+
+        [TestMethod]
+        public async Task Get_IsNotNull()
+        {
+            var result = await _controller.GetProducts();
+
+            Assert.IsNotNull(result, "Result is null");
+        }
+    }
+}
